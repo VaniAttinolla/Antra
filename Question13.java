0031c005d7aedc8e4f361d2dd80a5626c52be54a
VaniAttinolla <123376103+VaniAttinolla@users.noreply.github.com>
2023-01-23 18:16:26 +0530

    Add files via upload

--- Question13.java
+++ Question13.java
@@ -1,5 +1,19 @@
 package assignment_exception;
 
 public class Question13 {
-
+	 public static void main(String[] args) {
+			
+		 try
+	         {
+	             System.out.println("hello friends");
+	         }
+	         catch(Exception e)
+	         {            
+	             System.err.println(e);
+	         }
+	        
+	     }//ans :hello friends
 }
+
+
+
