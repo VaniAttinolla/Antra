d34a5f18cf3e77538b97858a6e145ac6505c3068
VaniAttinolla <123376103+VaniAttinolla@users.noreply.github.com>
2023-01-24 23:10:48 +0530

    Add files via upload

--- Question3.java
+++ Question3.java
@@ -1,14 +1,17 @@
-package assignment_exception;
+package assignment2;
 
 public class Question3 {
-	void f1() {
-		try {
-			
-		}
-		catch(Exception e) 
-		{
-			
-		}//ans:without try block we cannot use 
-	}
+	public static void main(String[] args)
+	{
+	try {
+	 int value = 20;
+	 if (value < 10)
+	 throw new RuntimeException("Value is less than 10");
+	 }
+	catch (RuntimeException re) {
+	 System.out.println(re.getMessage());
+	 }
+	System.out.println("Outside try-catch block");
+	 }//there is no exception print the outside try-catch block
 
 }
